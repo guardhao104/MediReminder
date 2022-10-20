201e474294ff0f68bbf536fd09d20a0afa91fda6
daniilmol <56126113+daniilmol@users.noreply.github.com>
2022-10-20 09:21:20 -0700

    added firebase

---
+++ app/src/main/java/com/example/medireminder/ReminderList.java
@@ -0,0 +1,43 @@
+package com.example.medireminder;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.medireminder.databinding.ActivityReminderListBinding;
+
+public class ReminderList extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityReminderListBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityReminderListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_reminder_list);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_reminder_list);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
