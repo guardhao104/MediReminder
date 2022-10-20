117b0aa9657e5190b3845c44baa270b4b3cc1ed4
daniilmol <56126113+daniilmol@users.noreply.github.com>
2022-10-20 09:47:08 -0700

    fixed gradle

--- app/src/main/java/com/example/medireminder/ReminderList.java
+++ app/src/main/java/com/example/medireminder/ReminderList.java
@@ -1,11 +1,17 @@
 package com.example.medireminder;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -14,12 +20,38 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.medireminder.databinding.ActivityReminderListBinding;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReminderList extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityReminderListBinding binding;
 
+    void x(){
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+        user.put("role", "patient");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +59,7 @@ public class ReminderList extends AppCompatActivity {
         binding = ActivityReminderListBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        //setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_reminder_list);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
