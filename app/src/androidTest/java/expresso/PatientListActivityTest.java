cdbcfa3f7a21159289f89cee03fb131f6618d1b2
lys7375 <yushuolu7375@gmail.com>
2022-11-28 02:49:26 -0800

    create UI tests for PatientListActivityTest.java

--- app/src/androidTest/java/expresso/PatientListActivityTest.java
+++ app/src/androidTest/java/expresso/PatientListActivityTest.java
@@ -2,6 +2,7 @@ package expresso;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
@@ -18,6 +19,8 @@ import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 
+import android.os.SystemClock;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -26,6 +29,7 @@ import com.example.medireminder.LandingPage;
 import com.example.medireminder.R;
 import com.example.medireminder.patient_info.PatientInfoListActivity;
 import com.example.medireminder.patient_list.PatientListActivity;
+import com.example.medireminder.patient_list.PatientListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Rule;
@@ -51,9 +55,29 @@ public class PatientListActivityTest {
     }
 
     // select list item, nav to detail
+    @Test
     public void test_selectListItemToDetail() {
-        onData(instanceOf(Map.class)).inAdapterView(withId(R.id.list_view_items))
-                .atPosition(1).perform(click());
+        SystemClock.sleep(1000);
+        onData(anything()).inAdapterView(withId(R.id.list_view_items)).atPosition(0).perform(click());
         onView((withId(R.id.patient_list_info))).check(matches(isDisplayed()));
+        pressBack();
+        onData(anything()).inAdapterView(withId(R.id.list_view_items)).atPosition(1).perform(click());
+        onView((withId(R.id.patient_list_info))).check(matches(isDisplayed()));
+        pressBack();
+        onData(anything()).inAdapterView(withId(R.id.list_view_items)).atPosition(2).perform(click());
+        onView((withId(R.id.patient_list_info))).check(matches(isDisplayed()));
+        pressBack();
+        onData(anything()).inAdapterView(withId(R.id.list_view_items)).atPosition(3).perform(click());
+        onView((withId(R.id.patient_list_info))).check(matches(isDisplayed()));
+        pressBack();
+        onData(anything()).inAdapterView(withId(R.id.list_view_items)).atPosition(4).perform(click());
+        onView((withId(R.id.patient_list_info))).check(matches(isDisplayed()));
+        pressBack();
+        onData(anything()).inAdapterView(withId(R.id.list_view_items)).atPosition(5).perform(click());
+        onView((withId(R.id.patient_list_info))).check(matches(isDisplayed()));
+        pressBack();
+        onData(anything()).inAdapterView(withId(R.id.list_view_items)).atPosition(6).perform(click());
+        onView((withId(R.id.patient_list_info))).check(matches(isDisplayed()));
+        pressBack();
     }
 }
